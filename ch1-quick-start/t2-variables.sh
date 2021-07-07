37394a9ffb433ba7aeb96a3f77833cbb7d20569d
Elroy Kanye <elroykanye@gmail.com>
2021-07-07 17:03:46 +0100

    for variables

---
+++ ch1-quick-start/t2-variables.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# SCRIPT: Variables
+# AUTHOR: Elroy Kanye
+# DATE: 
+# 1.1.A (Valid are A, B, D, T and P)
+#       (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: Linux
+#
+# PURPOSE: To show how to create and access variables.
+#
+# REV LIST:
+#       DATE: Date of Revision
+#       By: Author of the Modification
+#       Modification: Describe what was modified, new features, etc
+#
+# set -n    # Uncomment to check script syntax, without execution.
+#           # NoteL Do not forget to put comment back.
+# set -x    # Uncomment to debug this shell script
+#
+############################################################
+#       DEFINE FILES AND VARIABLES HERE
+############################################################
+#
+#   Always define variables here.
+#
+    VARIABLE=5 # Define variable by directly assigning.
+    echo "Variables"
+    echo "VARIABLE = $VARIABLE" # access variable by $ character.
+#
+#
+############################################################
+#       DEFINE FUNCTIONS HERE
+############################################################
+
+
+
+############################################################
+#       BEGINNING OF MAIN
+############################################################
+
+
+
+
+
+
+
+############################################################
+#       END OF SCRIPT
+############################################################
