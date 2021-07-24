7c855d69e4e1ab6b2568e32454ec78c621f47919
Elroy Kanye <elroykanye@gmail.com>
2021-07-24 21:49:32 +0100

    added file with 5

---
+++ ch1-quick-start/t5-user-info-commands.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# SCRIPT: User Info Commands
+# AUTHOR: Elroy Kanye
+# DATE: 
+# 1.1.A (Valid are A, B, D, T and P)
+#       (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: Linux
+#
+# PURPOSE: To show examples of user information from system commands
+#
+# REV LIST:
+#       DATE: Date of Revision
+#       By: Author of the Modification
+#       Modification: Describe what was modified, new features, etc
+#
+# set -n    # Uncomment to check script syntax, without execution.
+#           # NoteL Do not forget to put comment back.
+# set -x    # Uncomment to debug this shell script
+#
+############################################################
+#       DEFINE FILES AND VARIABLES HERE
+############################################################
+
+
+
+
+############################################################
+#       DEFINE FUNCTIONS HERE
+############################################################
+
+
+
+############################################################
+#       BEGINNING OF MAIN
+############################################################
+
+# who command
+#       this gives the output for each logged-in user: username,
+#       tty, login time and where the user logged in from
+#
+# w command
+#       this is an extended who and is more detailed.
+#
+# last command
+#       this is used to show the history of who has logged in to the
+#       the system since the wtmp file was created.
+#
+# ps command
+#       this is used to show the information about current system processes.
+
+
+
+
+############################################################
+#       END OF SCRIPT
+############################################################
